49aa2af3de46750bf2f2d835e79d200ce40b7b3c
Alex Maybee <aleksandr.mayboroda@gmail.com>
2019-05-15 09:55:43 +0300

    added custom title to resource event at calendar

--- Переносить встречу в нужный календарь и изменение названия при создании_изменении/local/php_interface/init.php
+++ Переносить встречу в нужный календарь и изменение названия при создании_изменении/local/php_interface/init.php
@@ -62,12 +62,18 @@ class ActionCalendar
             );
             $search = 'Бронювання';
             if(preg_match('/'.$search.'/',$arFields['NAME'])){
-                echo '<br>СОВПАДЕНИЕ!<br>';
-                $updFields['NAME'] =  preg_replace('/'.$search.'/','Співбесіда',$arFields['NAME']);
+
+            //UPD 14.05 - делаем название события календаря по габлону:
+            // $updFields['NAME'] = 'півбесіда: + название сделки, + ФИО Абитуриента, + ФИО родителей, + скайп/вайбер абитуриента, + примечание из сделки'
+                $newActivityTitle = self::formNewActivityTitle($arFields['ID']);
+
+
+           //     echo '<br>СОВПАДЕНИЕ!<br>';
+                $updFields['NAME'] = preg_replace('/'.$search.'/','Співбесіда',$arFields['NAME']).' - '.$newActivityTitle;
             }
             $upd = self::updateCalendarSection($updFields);
         }
-       // df([$arFields,$upd,$GLOBALS['_POST']]);
+       // df([$arFields,$newActivityTitle,$upd,$GLOBALS['_POST']]);
     }
 
     private function updateCalendarSection($arFields)
@@ -78,6 +84,108 @@ class ActionCalendar
         $res = CCalendarEvent::Edit($Params);
         //df($res);
     }
+
+    // $updFields['NAME'] = 'півбесіда: + название сделки, + ФИО Абитуриента, + ФИО родителей, + скайп/вайбер абитуриента, + примечание из сделки'
+    private function formNewActivityTitle($activityId){
+        $activityTitleString = false;
+        $abiturientMassengers = false;
+
+        $resEvents = CCalendarEvent::GetById($activityId);
+        if($resEvents['UF_CRM_CAL_EVENT']){
+            $dealID = false;
+
+            foreach($resEvents['UF_CRM_CAL_EVENT'] as $entityTypeId){
+                if(preg_match('/D_[\d]+/', $entityTypeId)) $dealID = substr($entityTypeId,2);
+            }
+
+            if($dealID){
+                $dealsMassive = self::getDealByFilter1(['ID' => $dealID],['ID','TITLE','COMPANY_ID','UF_CRM_1557824219221']);
+                if($dealsMassive){
+                    //Запрос массива контактов, которые привязаны к семье (компании) по ID
+                    $contacts = self::getContactsByFilter1(['COMPANY_ID' => $dealsMassive[0]['COMPANY_ID']],['ID','NAME','LAST_NAME','UF_CRM_5CA325B4E045E','COMPANY_ID']);
+
+                    if(count($contacts) > 0){
+                        foreach($contacts as $contact){
+//                            $newContactMassive[] = [
+//                                'NAME' => $contact['LAST_NAME'].' '.$contact['NAME'],
+//                                'ROLE' => getStatusName12($contact['UF_CRM_5CA325B4E045E']),
+//                                'ROLE_ID' => $contact['UF_CRM_5CA325B4E045E'],
+//                            ];
+
+                            if($contact['UF_CRM_5CA325B4E045E'] === 'PARTNER'){
+                                $activityTitleString .= $contact['LAST_NAME'].' '.$contact['NAME'].'('.self::getStatusName1($contact['UF_CRM_5CA325B4E045E']).'), ';
+                                //Получаем мессенджеры абитуриента - getIMcontact
+                                $massengersFilter = [
+                                    'ENTITY_ID'  => 'CONTACT',
+                                    'ELEMENT_ID' => $contact['ID'],
+                                    'TYPE_ID'    => 'IM', //PHONE,EMAIL
+                                    //'VALUE_TYPE' => 'WORK',
+                                ];
+                                $massengersSelect = ['VALUE_TYPE','VALUE'];
+                                $abiturientMassengers = self::getIMcontact($massengersFilter,$massengersSelect);
+                            }
+
+                            else $activityTitleString .= $contact['LAST_NAME'].' '.$contact['NAME'].'('.self::getStatusName1($contact['UF_CRM_5CA325B4E045E']).'), ';
+                        }
+                    }
+
+                    //Добавление мессенджеров абитуриента в строку
+                    if ($abiturientMassengers){
+                        $activityTitleString .= 'Зв\'язок: ';
+                        foreach ($abiturientMassengers as $massenger){
+                            $activityTitleString .= $massenger['VALUE'].' ('.$massenger['VALUE_TYPE'].'), ';
+                        }
+                    }
+
+                    //Ну и добавляем примечание в конец строки
+                    if($activityTitleString && $dealsMassive[0]['UF_CRM_1557824219221'] != '')
+                        $activityTitleString .= 'Примітка: '.$dealsMassive[0]['UF_CRM_1557824219221'];
+
+                }
+            }
+
+        }
+
+        return $activityTitleString;
+    }
+
+    private function getDealByFilter1($filter,$select){
+        $db_list = CCrmDeal::GetListEx(['ID' => 'ASC'], $filter, false, false, $select, array()); //получение пользовательских полей сделки по ID
+
+        $result = array();
+        while ($dealsList = $db_list->GetNext()) {
+            $result[] = $dealsList;
+        }
+
+        if($result) return $result;
+        return false;
+    }
+
+    private function getContactsByFilter1($filter,$select){
+        $contactData = CCrmContact::GetList(array('ID' => 'ASC'), $filter, $select, false);
+        $result = [];
+        while ($ar_result = $contactData->GetNext()){
+            $result[] = $ar_result;
+        }
+        return $result;
+    }
+
+    private function getStatusName1($ID){
+        $db_list = CCrmStatus::GetList(array(), array('ENTITY_ID' => 'CONTACT_TYPE', 'STATUS_ID' => $ID));
+        if ($ar_result = $db_list->GetNext())
+            return $ar_result['NAME'];
+    }
+
+    //получение IM (контакт- фейсбук, вайбер, телеграмм...)
+    private function getIMcontact($filter,$select){
+        $dbRes = \CCrmFieldMulti::GetListEx(['ID' => 'asc'], $filter, false, false, $select);
+        $allIm = false;
+        while ($multiFields = $dbRes->Fetch()) {
+            $allIm[] = $multiFields;
+        }
+        return $allIm;
+    }
+
 }
 
 
